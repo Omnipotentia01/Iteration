4ee20f8a00e6e952c3b3425effea80eac8bfac29
Omnipotentia01 <tommy_tlc@hotmail.co.uk>
2013-10-14 09:58:41 +0100

    Added Largest Number.py
    Added the Largest Number.py doc in working condition.

--- 8bit.py
+++ 8bit.py
@@ -2,11 +2,14 @@ import time
 count = int(input("Please enter the number of turtles you would like: "))
 
 for count in range (count):
-    print ("─▄▀▀▀▄────▄▀█▀▀█▄")
-    print ("▄▀─▀─▀▄▄▀█▄▀─▄▀─▄▀▄")
-    print ("█▄▀█───█─█▄▄▀─▄▀─▄▀▄")
-    print ("──█▄▄▀▀█▄─▀▀▀▀▀▀▀─▄█")
-    print ("─────▄████▀▀▀▀████─▀▄")
-    print ()
-    time.sleep(0.01)
+    if count < 0:
+        print("Give me {0} turtle(s)!".format(count * -1))
+    else:
+        print ("─▄▀▀▀▄────▄▀█▀▀█▄")
+        print ("▄▀─▀─▀▄▄▀█▄▀─▄▀─▄▀▄")
+        print ("█▄▀█───█─█▄▄▀─▄▀─▄▀▄")
+        print ("──█▄▄▀▀█▄─▀▀▀▀▀▀▀─▄█")
+        print ("─────▄████▀▀▀▀████─▀▄")
+        print ()
+        
     
---
+++ Largest Number.py
@@ -0,0 +1,9 @@
+#Tommy Cakebread
+#Largest Number
+old_number = 0
+new_number = None
+while new_number != -1:
+    new_number = int(input("Please enter a number: "))
+    if old_number < new_number:
+        old_number = new_number
+print("The largest number you typed was {0}".format(old_number))
