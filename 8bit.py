f2c8332bbaf7d0c230322a225ef958a2aa168393
Omnipotentia01 <tommy_tlc@hotmail.co.uk>
2013-10-14 09:42:55 +0100

    Manipulation to 8bit code.
    Added a feature that allows the user to input the number of turtles they
    want and it give them turtles.

--- 8bit.py
+++ 8bit.py
@@ -1,5 +1,12 @@
-print ("─▄▀▀▀▄────▄▀█▀▀█▄")
-print ("▄▀─▀─▀▄▄▀█▄▀─▄▀─▄▀▄")
-print ("█▄▀█───█─█▄▄▀─▄▀─▄▀▄")
-print ("──█▄▄▀▀█▄─▀▀▀▀▀▀▀─▄█")
-print ("─────▄████▀▀▀▀████─▀▄")
+import time
+count = int(input("Please enter the number of turtles you would like: "))
+
+for count in range (count):
+    print ("─▄▀▀▀▄────▄▀█▀▀█▄")
+    print ("▄▀─▀─▀▄▄▀█▄▀─▄▀─▄▀▄")
+    print ("█▄▀█───█─█▄▄▀─▄▀─▄▀▄")
+    print ("──█▄▄▀▀█▄─▀▀▀▀▀▀▀─▄█")
+    print ("─────▄████▀▀▀▀████─▀▄")
+    print ()
+    time.sleep(0.01)
+    
